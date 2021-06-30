6bb1807f57906489839ae1e27313d46f97d9417e
taidoan493 <86719685+taidoan493@users.noreply.github.com>
2021-06-30 18:50:32 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,22 +1,22 @@
 const CONFIG = {
-    introTitle: 'Em bé ơi :3',   
+    introTitle: 'Em bé à :3',   
     introDesc: `Trái đất vốn lạ thường
-    
+         
     Mà sao em cứ đi nhầm đường
-    
+         
     Lạc vào tim anh lẻ loi
-    
+         
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'Awwww',
+    btnIntro: 'Awwwwww',
     title: 'Do you love me at the first sight? ',
-    desc: '<3333 ',
+    desc: 'Ỏooooo :3 ',
     btnYes: 'Đúng ời anh yêu :3',
     btnNo: 'Ngáo!',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Tại Tài đẹp trai, dễ huông, cưng Thư nũa <3',
-    mess: 'Biết mà :3',
-    messDesc: 'Will you date with me? again ',
-    btnAccept: 'Okiiiii lun <3',
+    mess: 'Will you date with me? :3',
+    messDesc: 'Again?',
+    btnAccept: 'Okela <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
