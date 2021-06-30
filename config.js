d77b4f4cbbde06db51f41b9a1fb5a8774409775d
taidoan493 <86719685+taidoan493@users.noreply.github.com>
2021-06-30 18:56:50 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,14 @@
 const CONFIG = {
     introTitle: 'Em bé à :3',   
     introDesc: `Trái đất vốn lạ thường
-    .
     Mà sao em cứ đi nhầm đường
-    .
     Lạc vào tim anh lẻ loi
-    .
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'Awwwwww',
     title: 'Do you love me at the first sight? ',
     desc: 'Ỏooooo :3 ',
     btnYes: 'Đúng ời anh yêu :3',
-    btnNo: 'Ngáo!',
+    btnNo: 'Ngáo! Nói xạo đó',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Tại Tài đẹp trai, dễ huông, cưng Thư nũa <3',
